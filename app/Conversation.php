10841290a529152d97249a3864ef9703a6d508ea
Misha <msimakov661@gmail.com>
2020-03-15 16:31:13 +0300

    set up database and models

---
+++ app/Conversation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Conversation extends Model
+{
+    protected $dates = ['last_reply'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+    }
+
+    public function usersExceptCurrentlyAuthenticated()
+    {
+        return $this->user()->where('user_id', '!=', Auth::user()->id);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Conversation::class, 'parent_id')->latestFirst();
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Conversation::class, 'parent_id');
+    }
+
+    public function touchLastReply()
+    {
+        $this->last_reply = Carbon::now();
+        $this->save();
+    }
+
+    public function isReply()
+    {
+       return $this->parent_id !== null;
+    }
+
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
+}
